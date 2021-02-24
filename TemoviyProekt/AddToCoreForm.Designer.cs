66a6fe67c2a2218dd579181352ea1f701801f686
Artem2021-Step <mistoriwne030405@gmail.com>
2021-02-25 01:19:02 +0200

    First commit

---
+++ TemoviyProekt/AddToCoreForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace TreeViewProject
+{
+    partial class AddToCoreForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblMain = new System.Windows.Forms.Label();
+            this.btnSuccess = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.Location = new System.Drawing.Point(33, 68);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.PlaceholderText = "Поле для темок";
+            this.txtName.Size = new System.Drawing.Size(313, 34);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblMain
+            // 
+            this.lblMain.AutoSize = true;
+            this.lblMain.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMain.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMain.Location = new System.Drawing.Point(133, 9);
+            this.lblMain.Name = "lblMain";
+            this.lblMain.Size = new System.Drawing.Size(136, 28);
+            this.lblMain.TabIndex = 2;
+            this.lblMain.Text = "Додати темку";
+            // 
+            // btnSuccess
+            // 
+            this.btnSuccess.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSuccess.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSuccess.Location = new System.Drawing.Point(12, 137);
+            this.btnSuccess.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSuccess.Name = "btnSuccess";
+            this.btnSuccess.Size = new System.Drawing.Size(175, 46);
+            this.btnSuccess.TabIndex = 3;
+            this.btnSuccess.Text = "Хасанить";
+            this.btnSuccess.UseVisualStyleBackColor = true;
+            this.btnSuccess.Click += new System.EventHandler(this.btnSuccess_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(201, 137);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Уйти";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AddToCoreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 204);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSuccess);
+            this.Controls.Add(this.lblMain);
+            this.Controls.Add(this.txtName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "AddToCoreForm";
+            this.Text = "Додавання темки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblMain;
+        private System.Windows.Forms.Button btnSuccess;
+        private System.Windows.Forms.Button button1;
+    }
+}
